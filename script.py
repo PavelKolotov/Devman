be8e17baf4a9d6ecf95062fb71d311244f281c3c
Pavel_Kolotov <pavelkolotov84@gmail.com>
2023-01-11 14:59:44 +0300

    refactoring script.py

--- script.py
+++ script.py
@@ -1,10 +1,10 @@
 import requests
+from urllib.parse import urljoin
 
 weather_api = "https://wttr.in/"
 cities = ["Лондон", "Шереметьево", "Череповец"]
 payload = {"nTqM": "", "lang": "ru"}
 for city in cities:
-    response = requests.get(f'{weather_api} {city}', params=payload)
+    response = requests.get(urljoin(weather_api, city), params=payload)
     response.raise_for_status()
     print(response.text)
-
